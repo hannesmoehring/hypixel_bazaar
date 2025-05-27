d16369158f86180be8363fafc015c0d088709014
Hannes Möhring <acc@hannesmoehring.de>
2025-05-27 16:13:03 +0200

    asd

--- data_retrieval.py
+++ data_retrieval.py
@@ -4,33 +4,50 @@ from datetime import datetime as dt
 
 import requests
 
-API_URL = "https://api.hypixel.net/v2/skyblock/bazaar"
-DATA_DIR = "data"
+API_URL_MAYOR = "https://api.hypixel.net/v2/resources/skyblock/election"
+API_URL_BAZAAR = "https://api.hypixel.net/v2/skyblock/bazaar"
+DATA_DIR_BAZAAR = "abc/bz"
+DATA_DIR_MAYOR = "abc/mayor"
 
 
-def fetch_data_from_api(api):
-    response = requests.get(api)
-    return response
+def fetch_data_bazaar(api, time):
+    data = requests.get(api)
+    if data.status_code == 200:
+        print("Data fetched successfully for BAZAAR.")
+        data = data.json()
+        save_data_to_json(data, DATA_DIR_BAZAAR, time)
+        print("Data saved to JSON file.")
+    else:
+        print("Failed to fetch data for BAZAAR.")
+        print(f"Error: {data.status_code}")
 
 
-def save_data_to_json(data, time:str):
-    with open(f"{DATA_DIR}/data_{time}.json", "w") as outfile:
+def fetch_data_mayor(api, time):
+    data = requests.get(api)
+
+    if data.status_code == 200:
+        print("Data fetched successfully for MAYOR.")
+        data = data.json()
+        save_data_to_json(data, DATA_DIR_MAYOR, time)
+        print("Data saved to JSON file.")
+    else:
+        print(data)
+        print("Failed to fetch data for MAYOR.")
+        print(f"Error: {data.status_code}")
+
+
+
+def save_data_to_json(data, dir, time:str):
+    with open(f"{dir}/data_{time}.json", "w") as outfile:
         json.dump(data, outfile, indent=4)
 
 
 def routine():
     time = dt.now().strftime("%d-%m_%H-%M")
+ 
     print(f"Fetching data from API at {time}...")
-    data = fetch_data_from_api(API_URL)
-    if data.status_code == 200:
-        print("Data fetched successfully.")
-        data = data.json()
-        save_data_to_json(data, time)
-        print("Data saved to JSON file.")
-    else:
-        print("Failed to fetch data.")
-        print(f"Error: {data.status_code}")
-
+    fetch_data_bazaar(API_URL_BAZAAR, time)
+    fetch_data_mayor(API_URL_MAYOR, time)
     print("\n-----------------------------------------------------------------------\n")
 
 
