b164b2711ffc7a2c63fc414d1e4f7f6f540d13ed
Hannes Möhring <acc@hannesmoehring.de>
2025-06-10 23:19:49 +0200

    basic product api endpoint initialized

--- server/api_handler.py
+++ server/api_handler.py
@@ -1,15 +1,22 @@
+import os
+
+import pandas as pd
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 from utility import load_timeseries
 
 app = FastAPI(title="Minecraft-Stocks API")
 
-origins = ["http://localhost:3000", "https://your-frontend.tld"]
+dataframe = pd.read_pickle(os.path.join("data", "local_pickle", "cached_df.pkl"))
+
+origins = ["http://localhost:3000"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins, allow_credentials=True,
     allow_methods=["*"], allow_headers=["*"],
 )
+
+
 
 @app.get("/api/ticker")
 async def ticker(product: str = Query(...)):
@@ -17,3 +24,12 @@ async def ticker(product: str = Query(...)):
     return df.reset_index()             \
              .rename(columns={"index": "time"}) \
              .to_dict(orient="records")
+
+@app.get("/api/product/{prodId}")
+async def getProduct(prodId : str):
+    return load_timeseries(product=prodId, DF=dataframe)
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("api_handler:app", port=8000, reload=True)
--- server/utility.py
+++ server/utility.py
@@ -1,2 +1,13 @@
-def load_timeseries(product_id):
-    ...
+import pandas as pd
+
+
+def load_timeseries(product: str, DF: pd.DataFrame) -> list[dict]:  
+    cols = ['ds', 'inst_sellPrice', 'sellVolume',
+       'inst_sellPastWeek', 'sellOrders', 'inst_buyPrice', 'buyVolume',
+       'inst_buyPastWeek', 'buyOrders'
+    ]
+    df = DF.loc[DF["productId"] == product, cols].copy()
+
+
+    
+    return df.sort_values("ds").to_dict(orient="records")
